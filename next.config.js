92a79a822f85cee6a5ef33ffad179e4812f03f7c
Elizaveta <lizastepanova93@mail.ru>
2023-10-24 10:46:36 +0700

    Старт проекта, настроены конфиги Next, Tailwind. Создана структура проекта

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
+const { sources } = require('next/dist/compiled/webpack/webpack');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	poweredByHeader: false,
+	optimizeFonts: false,
+	env: {
+		APP_URL: process.env.NEXT_PUBLIC_REACT_API_URL,
+		APP_ENV: process.env.REACT_APP_ENV,
+	},
+	async rewrites() {
+		return [
+			{
+				source: '/api/:path*',
+				destination: 'http://localhost:4200/api/:path*',
+			},
+			{
+				source: '/uploads/:path*',
+				destination: 'http://localhost:4200/uploads/:path*',
+			},
+		];
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
